357c79ab821945fa666b8f9b86a7fde79fb53980
Sam <157414552+GrimCipher@users.noreply.github.com>
2024-01-23 13:07:25 -0600

    finished demo

---
+++ helloWorld.js
@@ -0,0 +1,27 @@
+console.log('Hello, World')
+
+const firstName = 'Sam'
+
+let age = 19
+
+console.log(`hello, my name is ${firstName} and i am ${age} years old.`)
+
+const isCool = false
+const hasDogs = true
+
+let height;
+let height2 = undefined
+
+height = '6ft'
+
+console.log(height)
+
+let scottAge = 25
+
+if (age > scottAge){
+    console.log(`i'm older than scott`)
+}else if(age === scottAge){
+    console.log(`i'm the same age as scott`)
+} else {
+    console.log(`i'm younger than scott`)
+}
